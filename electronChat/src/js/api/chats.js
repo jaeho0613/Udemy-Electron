bbedbdc9c0ca9d88c6eb89a8163e7b6cacdb46cf
jaeho0613 <ywm2004@gmail.com>
2023-08-14 16:32:01 +0900

    Section3 - 28. Dotenv

--- electronChat/src/js/api/chats.js
+++ electronChat/src/js/api/chats.js
@@ -1,10 +1,8 @@
 import db from "../db/firestore";
 
+const extractSnapshotData = (snapshot) =>
+  snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+
 export const fetchChats = () => {
-  return db
-    .collection("chats")
-    .get()
-    .then((snapshot) =>
-      snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() })),
-    );
+  return db.collection("chats").get().then(extractSnapshotData);
 };
--- electronChat/src/js/db/firestore.js
+++ electronChat/src/js/db/firestore.js
@@ -4,13 +4,13 @@ import "firebase/firestore";
 // TODO: Replace the following with your app's Firebase project configuration
 // See: https://support.google.com/firebase/answer/7015592
 const firebaseConfig = {
-  apiKey: "AIzaSyBWwH2Uex0eNjt_20nHW8FWVSYf7nuV_y0",
-  authDomain: "electron-chat-6a9e9.firebaseapp.com",
-  projectId: "electron-chat-6a9e9",
-  storageBucket: "electron-chat-6a9e9.appspot.com",
-  messagingSenderId: "976162580319",
-  appId: "1:976162580319:web:ae4fb2a5723281a174b8ea",
-  measurementId: "G-KGVTZPNW55",
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
