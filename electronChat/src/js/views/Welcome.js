ecbf6586d22ed42dfc7a534340d2ccf3e3d99234
jaeho0613 <ywm2004@gmail.com>
2023-08-16 22:20:46 +0900

    Section3 - 35. Toggling between forms

--- electronChat/src/js/views/Welcome.js
+++ electronChat/src/js/views/Welcome.js
@@ -1,15 +1,26 @@
 import LoginForm from "../components/LoginForm";
+import { useState } from "react";
+import RegisterForm from "../components/RegisterForm";
 
 const Welcome = () => {
+  const [isLoginView, setIsLoginView] = useState(true);
+
+  const optInText = isLoginView
+    ? ["Need an account?", "Register"]
+    : ["Already registered", "Login"];
+
   return (
     <>
       <div className="centered-view">
         <div className="centered-container">
-          <LoginForm />
+          {isLoginView ? <LoginForm /> : <RegisterForm />}
           <small className="form-text text-muted mt-2">
-            Already registered?
-            <span onClick={() => {}} className="btn-link ml-2">
-              Login
+            {optInText[0]}
+            <span
+              onClick={() => setIsLoginView(!isLoginView)}
+              className="btn-link ml-2"
+            >
+              {optInText[1]}
             </span>
           </small>
         </div>
