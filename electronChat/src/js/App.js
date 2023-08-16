ade953bc6b35943b82291e32739dbed10d72a550
jaeho0613 <ywm2004@gmail.com>
2023-08-16 22:14:51 +0900

    Section3 - 34. Welcome view

--- electronChat/src/js/App.js
+++ electronChat/src/js/App.js
@@ -3,8 +3,7 @@ import HomeView from "./views/Home";
 import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import SettingsView from "./views/Settings";
-import LoginView from "./views/Login";
-import RegisterView from "./views/Register";
+import WelcomeView from "./views/Welcome";
 import ChatView from "./views/Chat";
 import { Provider } from "react-redux";
 import { configureStore } from "./store";
@@ -17,11 +16,10 @@ const App = () => {
           <div className="content-wrapper">
             <Navbar />
             <Routes>
-              <Route path={"/"} element={<HomeView />} />
+              <Route path={"/"} element={<WelcomeView />} />
+              <Route path={"/home"} element={<HomeView />} />
               <Route path={"/chat/:id"} element={<ChatView />} />
               <Route path={"/settings"} element={<SettingsView />} />
-              <Route path={"/login"} element={<LoginView />} />
-              <Route path={"/register"} element={<RegisterView />} />
             </Routes>
           </div>
         </Router>
