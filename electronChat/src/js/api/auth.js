8da8009f63062a7e508bcd16ffd3de2feeb48249
jaeho0613 <ywm2004@gmail.com>
2023-08-19 15:02:52 +0900

    Section3 - 38. Create user profile

--- electronChat/src/js/api/auth.js
+++ electronChat/src/js/api/auth.js
@@ -1,11 +1,24 @@
+import db from "../db/firestore";
 import firebase from "firebase/app";
 import "firebase/auth";
 
-export const register = async ({ email, password }) => {
+export const createUserProfile = (userProfile) =>
+  db.collection("profiles").doc(userProfile.uid).set(userProfile);
+
+export const register = async ({ email, password, username, avatar }) => {
   try {
     const { user } = await firebase
       .auth()
       .createUserWithEmailAndPassword(email, password);
+
+    await createUserProfile({
+      uid: user.uid,
+      username,
+      email,
+      avatar,
+      joinedChats: [],
+    });
+
     return user;
   } catch (e) {
     return Promise.reject(e.message);
