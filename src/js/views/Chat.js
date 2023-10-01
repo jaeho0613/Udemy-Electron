3416f5073b1b12f9caf2be129e482bb0c9320f91
jaeho0613 <ywm2004@gmail.com>
2023-10-01 17:25:41 +0900

    [프로젝트 재 설정] - unsub 오류 (찾진 못함...) - 강의 프로젝트로 덮어쓰기

---
+++ src/js/views/Chat.js
@@ -0,0 +1,59 @@
+
+
+import React, { useEffect, useRef } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useParams } from "react-router-dom";
+
+import ChatUserList from '../components/ChatUsersList';
+import ChatMessagesList from '../components/ChatMessagesList';
+import ViewTitle from '../components/shared/ViewTitle';
+import { withBaseLayout } from '../layouts/Base';
+
+import { subscribeToChat, subscribeToProfile } from '../actions/chats';
+
+function Chat() {
+  const { id } = useParams();
+  const peopleWatchers = useRef({});
+  const dispatch = useDispatch();
+  const activeChat = useSelector(({chats}) => chats.activeChats[id])
+  const joinedUsers = activeChat?.joinedUsers;
+
+  useEffect(() => {
+    const unsubFromChat = dispatch(subscribeToChat(id));
+    return () => {
+      unsubFromChat();
+      unsubFromJoinedUsers();
+    }
+  }, [])
+
+  useEffect(() => {
+    joinedUsers && subscribeToJoinedUsers(joinedUsers);
+  }, [joinedUsers])
+
+  const subscribeToJoinedUsers = (jUsers) => {
+    jUsers.forEach(user => {
+      if(!peopleWatchers.current[user.uid]) {
+        peopleWatchers.current[user.uid] = dispatch(subscribeToProfile(user.uid))
+      }
+    })
+  }
+
+  const unsubFromJoinedUsers = () => {
+    Object.keys(peopleWatchers.current)
+      .forEach(id => peopleWatchers.current[id]())
+  }
+
+  return (
+    <div className="row no-gutters fh">
+      <div className="col-3 fh">
+        <ChatUserList users={activeChat?.joinedUsers} />
+      </div>
+      <div className="col-9 fh">
+        <ViewTitle text={`Channel ${activeChat?.name}`} />
+        <ChatMessagesList />
+      </div>
+    </div>
+  )
+}
+
+export default withBaseLayout(Chat, {canGoBack: true});
