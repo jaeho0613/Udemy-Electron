19d9dd0b26e354f87b3725bf44a6e3735c8ce9a7
jaeho0613 <ywm2004@gmail.com>
2023-10-09 18:12:42 +0900

    Section7. Messaging ~97 Store date in local storage

--- src/js/store/middlewares/app.js
+++ src/js/store/middlewares/app.js
@@ -11,6 +11,16 @@ export default store => next => action => {
         body: action.isOnline ? 'Online' : 'Offline'
       });
     }
+    case 'SETTINGS_UPDATE': {
+      const {setting, value} = action;
+      const currentSettings = localStorage.getItem('app-settings');
+      const parsedCurrentSettings = currentSettings ? JSON.parse(currentSettings) : {};
+
+      const settings = {...parsedCurrentSettings, [setting] : value};
+      const stringifySettings = JSON.stringify(settings);
+
+      localStorage.setItem('app-settings', stringifySettings);
+    }
     case 'AUTH_LOGOUT_SUCCESS': {
       const { messagesSubs } = store.getState().chats;
       if (messagesSubs) {
